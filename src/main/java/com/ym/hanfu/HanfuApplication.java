26cd839db3d654b2bad1e3700189760deb5114bc
lqm48330 <liuqingmiao@chebada.com>
2020-04-20 22:37:18 +0800

    hanfu

---
+++ src/main/java/com/ym/hanfu/HanfuApplication.java
@@ -0,0 +1,13 @@
+package com.ym.hanfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanfuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanfuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ym/hanfu/HanfuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ym.hanfu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanfuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
